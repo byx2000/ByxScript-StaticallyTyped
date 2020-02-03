a074151b87807ad1209dce55035d85866b4c9783
byx <18023699313@163.com>
2020-02-03 15:19:18 +0800

    添加测试用例

--- src/ByxParser/Test/ParserTest.cpp
+++ src/ByxParser/Test/ParserTest.cpp
@@ -35,7 +35,7 @@ void ByxParserTest::Run()
 	cout << endl;
 
 	// ÐéÄâ»ú²âÊÔ
-	int numVMCase = 17;
+	int numVMCase = 20;
 	for (int i = 1; i <= numVMCase; ++i)
 	{
 		string path = "src/ByxParser/Test/TestCase/VMTest/";
